0100035c00a1d12d7f21a65dfbe3f01a39cbd68e
painful00 <35652940+painful00@users.noreply.github.com>
2022-08-22 19:29:28 +0800

    Add files via upload

--- model/SimpleHGN.py
+++ model/SimpleHGN.py
@@ -99,11 +99,11 @@ def identical_map(x):
 
 class SimpleHGN_AUG_P(nn.Module):
 
-    def __init__(self, config, g, feature_sizes, category_index, target_category, label_num, dataset):
+    def __init__(self, config, g1, g2, feature_sizes, category_index, target_category, label_num, dataset):
         super().__init__()
         heads = [config.num_heads] * config.num_layers + [1]
 
-        self.model = SimpleHGN(config.edge_dim, len(g.etypes), [feature_sizes[category_index[target_category]]], config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop, config.negative_slope, config.residual, config.beta)
+        self.model = SimpleHGN(config.edge_dim, len(g1.etypes), [feature_sizes[category_index[target_category]]], config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop, config.negative_slope, config.residual, config.beta)
 
         self.look_up_table = []
         self.feature_sizes = feature_sizes
@@ -111,6 +111,8 @@ class SimpleHGN_AUG_P(nn.Module):
         self.target_category = target_category
         self.label_num = label_num
         self.config = config
+
+        self.mapping = nn.Linear(2*label_num, label_num)
 
         for i, size in enumerate(feature_sizes):
             if i == category_index[target_category]:
@@ -121,13 +123,21 @@ class SimpleHGN_AUG_P(nn.Module):
             self.look_up_table = [identical_map for _ in range(len(feature_sizes))]
 
 
-    def forward(self, g_aug):
+    def forward(self, g_aug, g_ori):
         feat_dict = {}
         for ntype in g_aug.ntypes:
             if ntype in self.category_index:
                 feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_aug.ndata["h"][ntype])
             else:
                 feat_dict[ntype] = g_aug.ndata["h"][ntype]
-        logits = self.model(g_aug, feat_dict)[self.target_category]
+        logits1 = self.model(g_aug, feat_dict)[self.target_category]
+
+        feat_dict = {}
+        for ntype in g_ori.ntypes:
+            if ntype in self.category_index:
+                feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_ori.ndata["h"][ntype])
+        logits2 = self.model(g_ori, feat_dict)[self.target_category]
+
+        logits = F.softmax(self.mapping(torch.cat((logits1,logits2), dim=-1)), dim=-1)
 
         return logits
