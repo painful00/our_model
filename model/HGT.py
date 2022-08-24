651913ce7796dd3a9bc30c8cf221d69f870b8f8c
painful00 <35652940+painful00@users.noreply.github.com>
2022-08-24 15:42:55 +0800

    Add files via upload

--- model/HGT.py
+++ model/HGT.py
@@ -13,23 +13,22 @@ from tqdm import trange
 from conf import Config
 import dgl
 from utils import load_data, score, EarlyStopping
-from openhgnn import SimpleHGN
+from openhgnn import HGT
 from rcvae_model import VAE
 import torch.nn as nn
 import copy
 
-
-class SimpleHGN_AUG(nn.Module):
+class HGT_AUG(nn.Module):
 
     def __init__(self, config, g, feature_sizes, category_index, target_category, label_num, dataset):
         super().__init__()
-        heads = [config.num_heads] * config.num_layers + [1]
+
         if config.is_augmentation:
-            self.model = SimpleHGN(config.edge_dim, len(g.etypes), [config.embedding_size * (len(config.arg_argmentation_type)) + feature_sizes[category_index[target_category]]],
-                                   config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop,
-                                   config.negative_slope, config.residual, config.beta)
+            self.model = HGT(feature_sizes[category_index[target_category]], label_num, config.num_heads, len(g.etypes), len(g.ntypes), config.num_layers, config.dropout, config.norm)
+
         else:
-            self.model = SimpleHGN(config.edge_dim, len(g.etypes), [feature_sizes[category_index[target_category]]], config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop, config.negative_slope, config.residual, config.beta)
+            self.model = HGT(feature_sizes[category_index[target_category]], label_num, config.num_heads, len(g.etypes), len(g.ntypes), config.num_layers, config.dropout, config.norm)
+
         self.look_up_table = []
         self.feature_sizes = feature_sizes
         self.category_index = category_index
@@ -53,6 +52,7 @@ class SimpleHGN_AUG(nn.Module):
                     self.look_up_table.append(nn.Linear(size, feature_sizes[category_index[target_category]]))
             if dataset == "yelp":
                 self.look_up_table = [identical_map for _ in range(len(feature_sizes))]
+
 
     def forward(self, g_ori, augmentated_features, augmentated_types, augmentated_num, method):
         feat_dict = {}
@@ -93,64 +93,6 @@ class SimpleHGN_AUG(nn.Module):
             logits = self.model(g_ori, feat_dict)[self.target_category]
         return logits
 
+
 def identical_map(x):
     return x
-
-
-class SimpleHGN_AUG_P(nn.Module):
-
-    def __init__(self, config, g1, g2, feature_sizes, category_index, target_category, label_num, dataset):
-        super().__init__()
-        heads = [config.num_heads] * config.num_layers + [1]
-
-        if config.is_augmentation:
-            self.model = SimpleHGN(config.edge_dim, len(g1.etypes)+len(g2.etypes), [feature_sizes[category_index[target_category]]], config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop, config.negative_slope, config.residual, config.beta)
-        else:
-            self.model = SimpleHGN(config.edge_dim, len(g.etypes), [feature_sizes[category_index[target_category]]],
-                                   config.hidden_dim, label_num, config.num_layers, heads, config.feat_drop,
-                                   config.negative_slope, config.residual, config.beta)
-
-
-        self.look_up_table = []
-        self.feature_sizes = feature_sizes
-        self.category_index = category_index
-        self.target_category = target_category
-        self.label_num = label_num
-        self.config = config
-
-        self.mapping = nn.Linear(2*label_num, label_num)
-
-        for i, size in enumerate(feature_sizes):
-            if i == category_index[target_category]:
-                self.look_up_table.append(identical_map)
-            else:
-                self.look_up_table.append(nn.Linear(size, feature_sizes[category_index[target_category]]))
-        if dataset == "yelp":
-            self.look_up_table = [identical_map for _ in range(len(feature_sizes))]
-
-
-    def forward(self, g_aug, g_ori):
-        feat_dict = {}
-        if self.config.is_augmentation:
-            for ntype in g_aug.ntypes:
-                if ntype in self.category_index:
-                    feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_aug.ndata["h"][ntype])
-                else:
-                    feat_dict[ntype] = g_aug.ndata["h"][ntype]
-            logits1 = self.model(g_aug, feat_dict)[self.target_category]
-
-            feat_dict = {}
-            for ntype in g_ori.ntypes:
-                if ntype in self.category_index:
-                    feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_ori.ndata["h"][ntype])
-            logits2 = self.model(g_ori, feat_dict)[self.target_category]
-
-            logits = F.softmax(self.mapping(torch.cat((logits1,logits2), dim=-1)), dim=-1)
-
-
-        else:
-            for ntype in g_ori.ntypes:
-                feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_ori.ndata["h"][ntype])
-            logits = self.model(g_ori, feat_dict)[self.target_category]
-
-        return logits
