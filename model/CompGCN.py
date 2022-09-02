1d2133a77eeca3096bb271a567a388767d926275
painful00 <35652940+painful00@users.noreply.github.com>
2022-09-02 15:07:26 +0800

    Add files via upload

--- model/CompGCN.py
+++ model/CompGCN.py
@@ -102,10 +102,20 @@ class CompGCN_AUG_P(nn.Module):
         super().__init__()
 
         if config.is_augmentation:
-            self.model = CompGCN(feature_sizes[category_index[target_category]], config.hidden_dim, label_num,
-                                  g2.etypes + g1.etypes, g2.num_nodes() + g1.num_nodes(),
-                                  len(g2.etypes) + len(g1.etypes), config.num_layers, config.comp_fn, config.dropout,
+            self.arg_num = 32
+            #self.model = CompGCN(feature_sizes[category_index[target_category]], config.hidden_dim, label_num,
+            #                      g2.etypes + g1.etypes, g2.num_nodes() + g1.num_nodes(),
+            #                      len(g2.etypes) + len(g1.etypes), config.num_layers, config.comp_fn, config.dropout,
+            #                      F.relu, True)
+            self.model1 = CompGCN(feature_sizes[category_index[target_category]], config.hidden_dim, self.arg_num,
+                                  g1.etypes, g1.num_nodes(),
+                                  len(g1.etypes), config.num_layers, config.comp_fn, config.dropout,
                                   F.relu, True)
+            self.model2 = CompGCN(feature_sizes[category_index[target_category]]+self.arg_num, config.hidden_dim, label_num,
+                                  g2.etypes, g2.num_nodes(),
+                                  len(g2.etypes), config.num_layers, config.comp_fn, config.dropout,
+                                  F.relu, True)
+
         else:
             self.model = CompGCN(feature_sizes[category_index[target_category]], config.hidden_dim, label_num,
                                  g2.etypes, g2.num_nodes(), len(g2.etypes), config.num_layers, config.comp_fn,
@@ -121,11 +131,12 @@ class CompGCN_AUG_P(nn.Module):
 
         self.mapping = nn.Linear(2*label_num, label_num)
 
+
         for i, size in enumerate(feature_sizes):
             if i == category_index[target_category]:
                 self.look_up_table.append(identical_map)
             else:
-                self.look_up_table.append(nn.Linear(size, feature_sizes[category_index[target_category]]))
+                self.look_up_table.append(nn.Linear(size, feature_sizes[category_index[target_category]]+self.arg_num))
         if dataset == "yelp":
             self.look_up_table = [identical_map for _ in range(len(feature_sizes))]
 
@@ -133,20 +144,35 @@ class CompGCN_AUG_P(nn.Module):
     def forward(self, g_aug, g_ori):
         feat_dict = {}
         if self.config.is_augmentation:
+            # for ntype in g_aug.ntypes:
+            #     if ntype in self.category_index:
+            #         feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_aug.ndata["h"][ntype])
+            #     else:
+            #         feat_dict[ntype] = g_aug.ndata["h"][ntype]
+            # logits1 = self.model(g_aug, feat_dict)[self.target_category]
+            #
+            # feat_dict = {}
+            # for ntype in g_ori.ntypes:
+            #     if ntype in self.category_index:
+            #         feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_ori.ndata["h"][ntype])
+            # logits2 = self.model(g_ori, feat_dict)[self.target_category]
+            #
+            # logits = F.softmax(self.mapping(torch.cat((logits1,logits2), dim=-1)), dim=-1)
             for ntype in g_aug.ntypes:
-                if ntype in self.category_index:
-                    feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_aug.ndata["h"][ntype])
-                else:
-                    feat_dict[ntype] = g_aug.ndata["h"][ntype]
-            logits1 = self.model(g_aug, feat_dict)[self.target_category]
+                feat_dict[ntype] = g_aug.ndata["h"][ntype]
+            logits1 = self.model1(g_aug, feat_dict)[self.target_category]
 
             feat_dict = {}
             for ntype in g_ori.ntypes:
-                if ntype in self.category_index:
+                if ntype == self.target_category:
+                    feat_dict[ntype] = torch.cat((g_ori.ndata["h"][ntype], logits1), dim=-1)
+                elif ntype in self.category_index:
                     feat_dict[ntype] = self.look_up_table[self.category_index[ntype]](g_ori.ndata["h"][ntype])
-            logits2 = self.model(g_ori, feat_dict)[self.target_category]
 
-            logits = F.softmax(self.mapping(torch.cat((logits1,logits2), dim=-1)), dim=-1)
+            logits2 = self.model2(g_ori, feat_dict)[self.target_category]
+
+            #logits = F.softmax(self.mapping(torch.cat((logits1,logits2), dim=-1)), dim=-1)
+            logits = logits2
 
 
         else:
